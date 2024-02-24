cc732b3b761c83ad8a213301c33e47d7bc900cbe
alazaradane369 <alazaradane418@gmail.com>
2024-02-24 17:36:29 +0300

    Editing icons folder

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,12 +3,10 @@ import chevronLeft from './chevron-left.svg'
 import cheveronRight from './chevron-right.svg'
 import copyrightSign from './copyright-sign.svg'
 import facebook from './facebook.svg'
-import hamburger from './hamburger.svg'
+import hamburger from './hamburger.png'
 import instagram from './instagram.svg'
 import support from './support.svg'
-import shieldTick from './shield-tick.svg'
-//import star from './star.svg'
-import truckFast from './truck-fast.svg'
+
 import twitter from './twitter.svg'
 import star from './star.png'
 
@@ -21,8 +19,8 @@ export {
     hamburger,
     instagram,
     support,
-    shieldTick,
+   
     star,
-    truckFast,
+
     twitter
 }
