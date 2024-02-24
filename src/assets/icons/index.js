dc64fe24e000178126b0c9fca97e0a7df14f6bb0
alazaradane369 <alazaradane418@gmail.com>
2024-02-24 19:23:32 +0300

    clearing unuse components

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -3,7 +3,7 @@ import chevronLeft from './chevron-left.svg'
 import cheveronRight from './chevron-right.svg'
 import copyrightSign from './copyright-sign.svg'
 import facebook from './facebook.svg'
-import hamburger from './hamburger.png'
+
 import instagram from './instagram.svg'
 import support from './support.svg'
 
