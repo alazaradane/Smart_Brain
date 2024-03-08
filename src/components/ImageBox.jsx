d782cfa0668e4ee008fdc4bd3fd01f980d531453
alazaradane <alazargithub369@gmail.com>
2024-03-08 20:41:25 +0300

    Adjust image box and bounding box styles.

--- src/components/ImageBox.jsx
+++ src/components/ImageBox.jsx
@@ -5,22 +5,20 @@ import '../face.css'
 const ImageBox = ({imageUrl, box}) => {
   return (
     <div className=' flex items-center justify-center'>
-      {imageUrl ? <img id='inputImage' src={imageUrl} alt="image" width={500} height={'auto'} /> : 
-      <div>
+      {imageUrl ? <img id='inputImage' src={imageUrl} alt="image" width='500px' height='auto'/> : 
+      <div className=' h-auto absolute'>
         <p className=' text-xl font-palanquin font-bold text-center m-3'>Insert Image</p>
-        <img src={NoImg} width={150} height={150}/> 
+        <img src={NoImg} width={150} height={150}/>
       </div>
       }
       <div className='bounding-box' 
         style={
           {
-           top: box.topRow,
-            bottom: box.bottomRow,
-            left: box.leftCol,
-            width: box.rightCol ,
-            position:"absolute"
+            top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol
           }
-        }></div>
+        }>        
+        </div>
+      
     </div>
   )
 }
