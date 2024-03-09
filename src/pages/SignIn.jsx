cfd00ab0439e3e22a703654ea22f874b40a26df1
alazaradane <alazargithub369@gmail.com>
2024-03-09 05:45:14 +0300

    Signin setup

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <section
+        id='#signin'
+    >
+        <div>
+            
+        </div>
+        <div>
+            <form action="#">
+                <input type="text" />
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default SignIn
